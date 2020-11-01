77345dee23064d89576df053a0adefa0dd227ae4
studio <shwoodley1@gmail.com>
2020-11-01 19:01:17 -0400

    update testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 
-import './App.css';
+import React, {useState} from 'react';
+import FlashcardList from './FlashcardList';
 
 function App() {
+  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
   return (
-   <h1>hello shane</h1>
+   <FlashcardList  flashcards={flashcards}/>
   );
 }
+
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: 'What is 2 + 2?',
+    answer: '4',
+    options: [
+      '2',
+      '3',
+      '4',
+      '5'
+    ]
+  },
+  {
+    id: 2,
+    question: 'Question 2?',
+    answer: 'Answer',
+    options: [
+      'Answer',
+      'Answer 1',
+      'Answer 2',
+      'Answer 3'
+    ]
+  },
+
+]
 
 export default App;
---
+++ src/Flashcard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Flashcard({ Flashcard }) {
+    return (
+        <div>
+           {Flashcard.question} 
+        </div>
+    )
+}
